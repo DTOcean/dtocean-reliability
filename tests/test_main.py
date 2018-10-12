b73e49f6f110309c60bd8b137e8066815a4a5c83
Mathew Topper <damm_horse@yahoo.co.uk>
2018-10-12 10:51:19 +0100

    Fix copyright notices and authors

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,28 @@
+# -*- coding: utf-8 -*-
 
+#    Copyright (C) 2016-2018 Mathew Topper
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
-import pprint
-import time
-from collections import Counter
+from collections import Counter # Required for eval of text files
 
 from dtocean_reliability.main import Variables, Main
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = os.path.join(this_dir, "..", "sample_data")
+
 
 def test_main():    
     '''Test that main generates a non empty output'''
