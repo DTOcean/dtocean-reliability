3e993082536db2da2f0b8bd547e18ee760acd964
Mathew Topper <damm_horse@yahoo.co.uk>
2018-10-12 10:17:15 +0100

    Ensure diff-cover exceeds requirements

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,39 +13,198 @@ data_dir = os.path.join(this_dir, "..", "sample_data")
 def test_main():    
     '''Test that main generates a non empty output'''
     
-    input_variables = Variables(20.0 * 365.25 * 24.0, # mission time in hours
-                                'tidefloat', # user-defined bill of materials
-                                eval(open(os.path.join(data_dir, 'dummydb.txt')).read()), #Options: 'tidefloat', 'tidefixed', 'wavefloat', 'wavefixed'
-                                0.4 * 20.0 * 365.25 * 24.0, # target mean time to failure in hours
-                                'multiplehubs', #Options: 'radial', 'singlesidedstring', 'doublesidedstring', 'multiplehubs' 
-                                eval(open(os.path.join(data_dir, 'dummyelechiereg8.txt')).read()), # electrical system hierarchy
-                                eval(open(os.path.join(data_dir, 'dummyelecbomeg6.txt')).read()), # electrical system bill of materials
-                                eval(open(os.path.join(data_dir, 'dummymoorhiereg8.txt')).read()), # mooring and foundation system hierarchy
-                                eval(open(os.path.join(data_dir, 'dummymoorbomeg6.txt')).read()), # mooring and foundation system bill of materials
-                                eval(open(os.path.join(data_dir, 'dummyuserhiereg6.txt')).read()), # dummy user-defined hierarchy
-                                eval(open(os.path.join(data_dir, 'dummyuserbomeg6.txt')).read())) # database
+    dummydb = eval(open(os.path.join(data_dir, 'dummydb.txt')).read())
+    dummyelechier = eval(open(os.path.join(data_dir,
+                                           'dummyelechiereg8.txt')).read())
+    dummyelecbom = eval(open(os.path.join(data_dir,
+                                          'dummyelecbomeg6.txt')).read())
+    dummymoorhier = eval(open(os.path.join(data_dir,
+                                           'dummymoorhiereg8.txt')).read())
+    dummymoorbom = eval(open(os.path.join(data_dir,
+                                          'dummymoorbomeg6.txt')).read())
+    dummyuserhier = eval(open(os.path.join(data_dir,
+                                           'dummyuserhiereg6.txt')).read())
+    dummyuserbom = eval(open(os.path.join(data_dir,
+                                          'dummyuserbomeg6.txt')).read())
+    
+    input_variables = Variables(20.0 * 365.25 * 24.0,
+                                'tidefloat',
+                                dummydb,
+                                0.4 * 20.0 * 365.25 * 24.0, 
+                                'multiplehubs',
+                                dummyelechier,
+                                dummyelecbom,
+                                dummymoorhier,
+                                dummymoorbom,
+                                dummyuserhier,
+                                dummyuserbom)
                                 
     test = Main(input_variables)
-    mttf, rsystime = test()    
+    mttf, rsystime = test()
     
     assert mttf
     assert rsystime is not None
-    
-def test_solo_electrical():    
+
+
+def test_solo_electrical_radial():
     '''Test that main generates a non empty output for just the electrical
-    network
+    network, with a radial layout.
     '''
+
+    dummydb = eval(open(os.path.join(data_dir, 'dummydb1.txt')).read())
+    dummyelechier = eval(open(os.path.join(data_dir,
+                                           'dummyelechiereg9.txt')).read())
+    dummyelecbom = eval(open(os.path.join(data_dir,
+                                          'dummyelecbomeg9.txt')).read())
     
-    input_variables = Variables(20.0 * 365.25 * 24.0, # mission time in hours
-                                'tidefloat', # user-defined bill of materials
-                                eval(open(os.path.join(data_dir, 'dummydb1.txt')).read()), #Options: 'tidefloat', 'tidefixed', 'wavefloat', 'wavefixed'
-                                0.4 * 20.0 * 365.25 * 24.0, # target mean time to failure in hours
-                                'radial', #Options: 'radial', 'singlesidedstring', 'doublesidedstring', 'multiplehubs' 
-                                eval(open(os.path.join(data_dir, 'dummyelechiereg9.txt')).read()), # electrical system hierarchy
-                                eval(open(os.path.join(data_dir, 'dummyelecbomeg9.txt')).read())) # database
+    input_variables = Variables(20.0 * 365.25 * 24.0, 
+                                'tidefloat',
+                                dummydb,
+                                0.4 * 20.0 * 365.25 * 24.0,
+                                'radial',
+                                dummyelechier,
+                                dummyelecbom)
                                 
     test = Main(input_variables)
-    mttf, rsystime = test()    
+    mttf, rsystime = test()
+    
+    assert mttf
+    assert rsystime is not None
+
+
+def test_solo_moorings():
+    '''Test that main generates a non empty output for just the moorings
+    network
+    '''
+
+    dummydb = eval(open(os.path.join(data_dir, 'dummydb.txt')).read())
+    dummymoorhier = eval(open(os.path.join(data_dir,
+                                           'dummymoorhiereg8.txt')).read())
+    dummymoorbom = eval(open(os.path.join(data_dir,
+                                          'dummymoorbomeg6.txt')).read())
+    
+    input_variables = Variables(20.0 * 365.25 * 24.0, 
+                                'tidefloat',
+                                dummydb,
+                                0.4 * 20.0 * 365.25 * 24.0,
+                                'multiplehubs',
+                                moorfoundhierdict=dummymoorhier,
+                                moorfoundbomdict=dummymoorbom)
+                                
+    test = Main(input_variables)
+    mttf, rsystime = test()
+    
+    assert mttf
+    assert rsystime is not None
+
+
+def test_solo_user():
+    '''Test that main generates a non empty output for just the user
+    network
+    '''
+
+    dummydb = eval(open(os.path.join(data_dir, 'dummydb.txt')).read())
+    dummyuserhier = eval(open(os.path.join(data_dir,
+                                           'dummyuserhiereg6.txt')).read())
+    dummyuserbom = eval(open(os.path.join(data_dir,
+                                          'dummyuserbomeg6.txt')).read())
+    
+    input_variables = Variables(20.0 * 365.25 * 24.0, 
+                                'tidefloat',
+                                dummydb,
+                                0.4 * 20.0 * 365.25 * 24.0,
+                                'multiplehubs',
+                                userhierdict=dummyuserhier,
+                                userbomdict=dummyuserbom)
+                                
+    test = Main(input_variables)
+    mttf, rsystime = test()
+    
+    assert mttf
+    assert rsystime is not None
+
+
+def test_solo_user_no_array():
+    '''Test that main generates a non empty output for just the user
+    network without an array key in the hierarchy
+    '''
+
+    dummydb = eval(open(os.path.join(data_dir, 'dummydb.txt')).read())
+    dummyuserhier = eval(open(os.path.join(data_dir,
+                                           'dummyuserhiereg8.txt')).read())
+    dummyuserbom = eval(open(os.path.join(data_dir,
+                                          'dummyuserbomeg6.txt')).read())
+    
+    input_variables = Variables(20.0 * 365.25 * 24.0, 
+                                'tidefloat',
+                                dummydb,
+                                0.4 * 20.0 * 365.25 * 24.0,
+                                'multiplehubs',
+                                userhierdict=dummyuserhier,
+                                userbomdict=dummyuserbom)
+                                
+    test = Main(input_variables)
+    mttf, rsystime = test()
+    
+    assert mttf
+    assert rsystime is not None
+
+
+def test_electrical_user_no_array():
+    '''Test that main generates a non empty output'''
+    
+    dummydb = eval(open(os.path.join(data_dir, 'dummydb.txt')).read())
+    dummyelechier = eval(open(os.path.join(data_dir,
+                                           'dummyelechiereg8.txt')).read())
+    dummyelecbom = eval(open(os.path.join(data_dir,
+                                          'dummyelecbomeg6.txt')).read())
+    dummyuserhier = eval(open(os.path.join(data_dir,
+                                           'dummyuserhiereg8.txt')).read())
+    dummyuserbom = eval(open(os.path.join(data_dir,
+                                          'dummyuserbomeg6.txt')).read())
+    
+    input_variables = Variables(20.0 * 365.25 * 24.0,
+                                'tidefloat',
+                                dummydb,
+                                0.4 * 20.0 * 365.25 * 24.0, 
+                                'multiplehubs',
+                                dummyelechier,
+                                dummyelecbom,
+                                userhierdict=dummyuserhier,
+                                userbomdict=dummyuserbom)
+                                
+    test = Main(input_variables)
+    mttf, rsystime = test()
+    
+    assert mttf
+    assert rsystime is not None
+
+
+def test_moorings_user_no_array():
+    '''Test that main generates a non empty output'''
+    
+    dummydb = eval(open(os.path.join(data_dir, 'dummydb.txt')).read())
+    dummymoorhier = eval(open(os.path.join(data_dir,
+                                           'dummymoorhiereg8.txt')).read())
+    dummymoorbom = eval(open(os.path.join(data_dir,
+                                          'dummymoorbomeg6.txt')).read())
+    dummyuserhier = eval(open(os.path.join(data_dir,
+                                           'dummyuserhiereg8.txt')).read())
+    dummyuserbom = eval(open(os.path.join(data_dir,
+                                          'dummyuserbomeg6.txt')).read())
+    
+    input_variables = Variables(20.0 * 365.25 * 24.0,
+                                'tidefloat',
+                                dummydb,
+                                0.4 * 20.0 * 365.25 * 24.0, 
+                                'multiplehubs',
+                                moorfoundhierdict=dummymoorhier,
+                                moorfoundbomdict=dummymoorbom,
+                                userhierdict=dummyuserhier,
+                                userbomdict=dummyuserbom)
+                                
+    test = Main(input_variables)
+    mttf, rsystime = test()
     
     assert mttf
     assert rsystime is not None
