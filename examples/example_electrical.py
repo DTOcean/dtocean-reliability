5e76fece717b3fbe710c56dd5f099ff987a565d5
Mathew Topper <damm_horse@yahoo.co.uk>
2019-03-08 09:40:55 +0000

    Remove non-working examples and unused example data and rename remaining

--- examples/example_electrical.py
+++ examples/example_electrical.py
@@ -1,20 +1,16 @@
 
-
 import os
 import pprint
-import time
 from collections import Counter
-
-import numpy as np
-import matplotlib.pylab as plt
 
 from dtocean_reliability import start_logging
 from dtocean_reliability.main import Variables, Main
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
-data_dir = os.path.join(this_dir, "..", "sample_data")
+data_dir = os.path.join(this_dir, "..", "example_data")
 
-def main():    
+
+def main():
     '''Test that main generates a non empty output'''
     
     input_variables = Variables(20.0 * 365.25 * 24.0, # mission time in hours
@@ -24,20 +20,13 @@ def main():
                                 'radial', #Options: 'radial', 'multiplehubs' 
                                 eval(open(os.path.join(data_dir, 'dummyelechiereg9.txt')).read()), # electrical system hierarchy
                                 eval(open(os.path.join(data_dir, 'dummyelecbomeg9.txt')).read())) # electrical bom
-                                
-    test = Main(input_variables)    
+    
+    test = Main(input_variables)
     mttf, rsystime = test()
     
     return mttf, rsystime
-    
-def plot(rsystime):
-    
-    data = np.array(rsystime)
-    plt.plot(data[:,0], data[:,1])
-    plt.ylabel('System reliability', fontsize=10)
-    plt.xlabel('Time [hours]', fontsize=10)     
-    plt.show()
-    
+
+
 if __name__ == "__main__":
 
     start_logging(level="DEBUG")
@@ -46,5 +35,3 @@ if __name__ == "__main__":
     
     pprint.pprint('Mean time to failure (hours): {}'.format(mttf))
     pprint.pprint('Mean time to failure (years): {}'.format(mttf/(24.0*365.25)))
-    plot(rsystime)
-
--- examples/example_moorings.py
+++ examples/example_moorings.py
@@ -1,20 +1,16 @@
 
-
 import os
 import pprint
-import time
 from collections import Counter
-
-import numpy as np
-import matplotlib.pylab as plt
 
 from dtocean_reliability import start_logging
 from dtocean_reliability.main import Variables, Main
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
-data_dir = os.path.join(this_dir, "..", "sample_data")
+data_dir = os.path.join(this_dir, "..", "example_data")
 
-def main():    
+
+def main():
     '''Test that main generates a non empty output'''
     
     input_variables = Variables(20.0 * 365.25 * 24.0, # mission time in hours
@@ -22,24 +18,17 @@ def main():
                                 eval(open(os.path.join(data_dir, 'dummydb1.txt')).read()), #Options: 'tidefloat', 'tidefixed', 'wavefloat', 'wavefixed'
                                 0.4 * 20.0 * 365.25 * 24.0, # target mean time to failure in hours
                                 'radial', #Options: 'radial', 'singlesidedstring', 'doublesidedstring', 'multiplehubs' 
-                                eval(open(os.path.join(data_dir, 'dummyelechiereg9.txt')).read()), # electrical system hierarchy
-                                eval(open(os.path.join(data_dir, 'dummyelecbomeg9.txt')).read()), # database
+                                None,#eval(open(os.path.join(data_dir, 'dummyelechiereg9.txt')).read()), # electrical system hierarchy
+                                None,#eval(open(os.path.join(data_dir, 'dummyelecbomeg9.txt')).read()), # database
                                 eval(open(os.path.join(data_dir, 'dummymoorhiereg10.txt')).read()), # mooring system hierarchy
                                 eval(open(os.path.join(data_dir, 'dummymoorbomeg10.txt')).read())) # database
-                                
-    test = Main(input_variables)    
+    
+    test = Main(input_variables)
     mttf, rsystime = test()
     
     return mttf, rsystime, test
-    
-def plot(rsystime):
-    
-    data = np.array(rsystime)
-    plt.plot(data[:,0], data[:,1])
-    plt.ylabel('System reliability', fontsize=10)
-    plt.xlabel('Time [hours]', fontsize=10)     
-    plt.show()
-    
+
+
 if __name__ == "__main__":
 
     start_logging(level="DEBUG")
@@ -48,7 +37,5 @@ if __name__ == "__main__":
     
     pprint.pprint(['Mean time to failure (hours)',mttf])
     pprint.pprint(['Mean time to failure (years)',mttf/(24.0*365.25)])
-#    plot(rsystime)
 
-    pprint.pprint(test.rsubsysvalues2)
-    pprint.pprint(test.rsubsysvalues3)
+    pprint.pprint(test.rarrayvalue2)
