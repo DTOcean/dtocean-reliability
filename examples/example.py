455cab0a443b104cd774de36d8a0fcadbc0ee4ef
Mathew Topper <damm_horse@yahoo.co.uk>
2019-03-08 09:42:20 +0000

    More example modification and change target for tests

--- examples/example.py
+++ examples/example.py
@@ -1,20 +1,16 @@
 
-
 import os
 import pprint
-import time
 from collections import Counter
-
-import numpy as np
-import matplotlib.pylab as plt
 
 from dtocean_reliability import start_logging
 from dtocean_reliability.main import Variables, Main
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
-data_dir = os.path.join(this_dir, "..", "sample_data")
+data_dir = os.path.join(this_dir, "..", "example_data")
 
-def main():    
+
+def main():
     '''Test that main generates a non empty output'''
     
     input_variables = Variables(20.0 * 365.25 * 24.0, # mission time in hours
@@ -33,15 +29,8 @@ def main():
     mttf, rsystime = test()
     
     return mttf, rsystime
-    
-def plot(rsystime):
-    
-    data = np.array(rsystime)
-    plt.plot(data[:,0], data[:,1])
-    plt.ylabel('System reliability', fontsize=10)
-    plt.xlabel('Time [hours]', fontsize=10)     
-    plt.show()
-    
+
+
 if __name__ == "__main__":
 
     start_logging(level="DEBUG")
@@ -50,5 +39,3 @@ if __name__ == "__main__":
     
     pprint.pprint('Mean time to failure (hours): {}'.format(mttf))
     pprint.pprint('Mean time to failure (years): {}'.format(mttf/(24.0*365.25)))
-    plot(rsystime)
-
